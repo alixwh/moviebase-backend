750364159611878e61a760456e279882d5997e9a
ali <hahinn@ttu.ee>
2022-11-04 15:30:11 +0200

    Add service package

---
+++ src/main/java/ee/taltech/iti0302/webproject/api/service/MovieExternalService.java
@@ -0,0 +1,33 @@
+package ee.taltech.iti0302.webproject.api.service;
+
+import ee.taltech.iti0302.webproject.dto.MovieDto;
+import ee.taltech.iti0302.webproject.entities.Movie;
+import ee.taltech.iti0302.webproject.mapper.MovieMapper;
+import ee.taltech.iti0302.webproject.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class MovieExternalService {
+    private final MovieRepository movieRepository;
+    private final MovieMapper movieMapper;
+
+    public List<MovieDto> findAll() {
+        return movieMapper.toDtoList(movieRepository.findAll());
+    }
+
+    public MovieDto findById(int id) {
+        return movieMapper.toDto(movieRepository.findById(id).orElse(null));
+    }
+
+    public boolean isMovieInDatabase(Integer movieId) {
+        return movieRepository.existsById(movieId);
+    }
+
+    public Movie save(Movie movie) {
+        return movieRepository.save(movie);
+    }
+}
