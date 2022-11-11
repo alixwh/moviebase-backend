444e0cc398c6b2c85cb622483c40493517112d64
brkerg <brkerg@ttu.ee>
2022-11-11 16:34:03 +0200

    Add the curly bracket

--- src/main/java/ee/taltech/iti0302/webproject/controller/MovieController.java
+++ src/main/java/ee/taltech/iti0302/webproject/controller/MovieController.java
@@ -29,6 +29,11 @@ public class MovieController {
         return movieService.findAll();
     }
 
+    @GetMapping("movies/year/{releaseYear}")
+    public List<MovieDto> getMoviesByReleaseYear(@PathVariable("releaseYear") int year) {
+        return movieService.findByYear(year);
+    }
+
     @GetMapping("actor/{id}")
     public List<MovieDto> getMoviesByActor(@PathVariable("id") int actorId) {
         return actorService.findMoviesByActorId(actorId);
